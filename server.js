620412329c2d2972b297dd4ad9caf92abf81023d
A.N.P Jayamanna <niluprasadi12@gmail.com>
2023-07-19 16:49:22 +0530

    node api

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
-// const port = 3000;
+const port = 3000;
 
 app.get('/', (req,res) => {
     res.send('Hello Bhathiya');
 });
 
-// app.listen(port, () => {
-//     console.log(`Node app is running on port ${port}`);
-// });
+app.get('/blog', (req,res) => {
+    res.send('Hello Prasai');
+})
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Node app is running on port ${port}`);
+});
+
+// app.listen(3000);

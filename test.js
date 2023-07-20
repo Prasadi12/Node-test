08536696e699a9e9b67d054d26d759e4ab2b6811
A.N.P Jayamanna <niluprasadi12@gmail.com>
2023-07-20 11:37:20 +0530

    node api

--- test.js
+++ test.js
@@ -1,42 +1,54 @@
+//////////////server.js
+
 // const express = require('express');
 // const mongoose = require('mongoose');
+// const Employee = require('./Models/empModel')
 // const app = express();
 // const port = 3000;
 
-// app.get('/', (req,res) => {
-//     res.send('Hello Prasadi...!');
-// });
+// //middleware
+// app.use(express.json());
 
-// app.listen(port, ()=>{
-//     console.log(`Node app is running on port ${port}`)
-// });
+// app.post('/emp', async(req,res) => {
+//     try {
+//       const employee =  await Employee.create(req.body);
+//       res.status(200).json(employee);
+//     } catch (error) {
+//        console.log(error.message);
+//        res.status(500).json({message: error.message});
+//     }
+// })
 
+// //connected to the database
 // mongoose.connect('mongodb+srv://admin:988470864vV@cluster1.h8edhfd.mongodb.net/Employees?retryWrites=true&w=majority')
 // .then(() => {
-//     console.log('Connected to the mongoDB.');
+//     console.log('Connected to the mongDB.');
+//     app.listen(port, () => {
+//         console.log(`NodeApi is running on port ${port}`);
+//     });
 // }).catch((error) => {
 //     console.log(error);
-// });
+// })
 
 
+//////////////empModel
 
-//////////////////////////////////
+// const mongoose = require('mongoose');
 
-// const mongoose = require('mongoose')
-
-// const empSchema = mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: [true, 'Enter your name'],
-//     },
-//     age: {
-//         type: Number,
-//         required: true,
+// const empSchema = mongoose.Schema(
+//     {
+//         name: {
+//             type: String,
+//             required: [true, 'Name is required'],
+//         },
+//         age: {
+//             type: Number,
+//             required: true,
+//         }
+//     },{
+//         timestamps: true,
 //     }
-// },{
-//     timestamps: true,
-// }
 // );
 
-// const employee = mongoose.model('employee', empSchema)
-// model.export = employee;
+// const Employee = mongoose.model('Employee', empSchema);
+// module.exports = Employee;

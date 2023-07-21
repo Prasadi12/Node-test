84d8be76c2ad528a6232c0a8eb3bac45f97691de
A.N.P Jayamanna <niluprasadi12@gmail.com>
2023-07-21 09:06:46 +0530

    node api

--- server.js
+++ server.js
@@ -13,6 +13,52 @@ app.get('/emp', async(req,res) => {
     } catch (error) {
         res.status(500).json({message: error.message})
     }
+});
+
+app.get('/emp/:id', async(req,res) => {
+    try {
+        const {id} = req.params;
+        const employee = await Employee.findById(id);
+        res.status(200).json(employee);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/emp/create', async(req,res) => {
+    try {
+        const employee = await Employee.create(req.body);
+        res.status(200).json(employee)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.put('/emp/update/:id', async(req,res) =>{
+    try {
+        const {id} = req.params;
+        const employee = await Employee.findByIdAndUpdate(id, req.body);
+        if(!employee){
+            return res.status(404).json(`Can not find any emplyee with id ${id}`)
+        }
+        const updatedEmp = await Employee.findById(id);
+        res.status(200).json(updatedEmp)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.delete('/emp/:id', async(req,res) =>{
+    try {
+        const {id} = req.params;
+        const employee = await Employee.findByIdAndDelete(id)
+        if(!employee){
+            return res.status(404).json(`Can not find any employee with id ${id}`)
+        }
+        res.status(200).json(employee)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
 
 mongoose.connect('mongodb+srv://admin:988470864vV@cluster1.h8edhfd.mongodb.net/Employees?retryWrites=true&w=majority')

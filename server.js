645586dd357ae6c355bd26cd9347a318a5ab1135
A.N.P Jayamanna <niluprasadi12@gmail.com>
2023-07-21 08:53:24 +0530

    node api

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const port = 3000;
 
 app.use(express.json());
 
+app.get('/emp', async(req,res) => {
+    try {
+        const employee = await Employee.find({});
+        res.status(200).json(employee);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 mongoose.connect('mongodb+srv://admin:988470864vV@cluster1.h8edhfd.mongodb.net/Employees?retryWrites=true&w=majority')
 .then(()=>{
     console.log('Connected to the mongoDB')
